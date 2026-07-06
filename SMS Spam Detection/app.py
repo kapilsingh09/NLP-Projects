636524346d7804e18ad22ca639a0f629ded43943
kapilsingh09 <karan404katyura@gmail.com>
2026-07-06 09:53:06 +0530

    complete app deployed

---
+++ SMS Spam Detection/app.py
@@ -0,0 +1,73 @@
+﻿from flask import Flask, render_template, request
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+app = Flask(__name__)
+
+# Download NLTK resources
+nltk.download("punkt", quiet=True)
+nltk.download("stopwords", quiet=True)
+
+ps = PorterStemmer()
+
+#Text Preprocessing 
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words("english") and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open("vectorizer.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
+
+def predict_spam_ham(text):
+    transformed_sms = transform_text(text)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+
+    if result == 1:
+        return "Spam"
+    else:
+        return "Ham"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    message = request.form["message"]
+
+    prediction = predict_spam_ham(message)
+
+    return render_template(
+        "index.html",
+        prediction=prediction,
+        message=message
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ SMS Spam Detection/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+if model is None or vectorizer is None:
+#     raise ValueError("Model or vectorizer is not loaded properly.")
